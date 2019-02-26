14562349813d3fa136809e594f473ab65de94ac3
Amine <amsted34@gmail.com>
2019-02-26 12:48:18 +0100

    mongoDB performance Tweak

--- MongoDBPerformance/Program.cs
+++ MongoDBPerformance/Program.cs
@@ -12,7 +12,8 @@ namespace MongoDBPerformance
     {
         static void Main(string[] args)
         {
-            var client = new MongoClient("mongodb://localhost:27017");
+            WriteConcern WCvalue = new WriteConcern(0,null,false,false);  
+            var client = new MongoClient("mongodb://127.0.0.1:27017").WithWriteConcern(WCvalue);
 
             var database = client.GetDatabase("MS3");
             bool isMongoLive = database.RunCommandAsync((Command<BsonDocument>)"{ping:1}").Wait(1000);
@@ -20,29 +21,35 @@ namespace MongoDBPerformance
 
             var collec = database.GetCollection<BsonDocument>("datas");
             long count = 0;
-
+           
             Stopwatch st1 = new Stopwatch();
-            st1.Start();
-            long previousTime = st1.ElapsedMilliseconds;
+            Stopwatch st2 = new Stopwatch();
+            st2.Start(); 
             for (long i = 0; i < 5000000; i++)
             {
+                st1.Restart();
+               
                 var doc = new BsonDocument
             {
                 { "camera", "1" },
                 { "data", "1254874458774587456985" },
                 { "count", i.ToString() },
-                { "elapsed", st1.ElapsedMilliseconds - previousTime },
+                { "elapsed", st1.Elapsed.TotalMilliseconds},
                 { "date", DateTime.Now}
-
             };
-                collec.InsertOne(doc);
-                
-                previousTime = st1.ElapsedMilliseconds;
-                Console.WriteLine("count " + count++);
-                Thread.Sleep(1);
+
+                collec.InsertOneAsync(doc);
+
+               
+                Console.WriteLine("count " + count++ + " " + st1.Elapsed.TotalMilliseconds);
+               
+
+                //Thread.Sleep(1);
 
             }
-            Console.WriteLine(st1.ElapsedMilliseconds.ToString());
+            Console.WriteLine(st2.Elapsed.TotalSeconds.ToString() + " Seconds");
+            Console.WriteLine(st2.Elapsed.TotalMinutes.ToString() + " minutes");
+            Console.WriteLine(st2.Elapsed.TotalHours.ToString() + " Hours");
             Console.WriteLine(DateTime.Now);
             Console.ReadKey();
         }
